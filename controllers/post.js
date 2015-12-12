742356d221ff70e61d27c2c2dfcc1160a8ceb726
minatoth <thaniatkm@hotmail.com>
2015-12-12 09:50:09 -0400

    examen final

--- controllers/post.js
+++ controllers/post.js
@@ -5,13 +5,37 @@
 var models = require('../models/model.js');
 
 exports.registroPost = function (req, res){
-	var post = models.Posts.build({ nombres: "Nombres",
+	var post = models.Posts.build({ nombres: "nombre",
 									post: "post"
 									});
 
 	res.render('registroPost', {titulo:'Registro de Post', post: post});
 }
+exports.respuestaPost = function (req, res){ 
 
+res.render('respuestaPost', {mensaje:'post recibido'});
+
+}
+exports.guardarPost = function(req, res){ 
+
+console.log("ingresa a guardar"); 
+
+var contacto = models.Post.build (req.body.post);
+
+    contacto.save ({fields:["nombre",
+
+      "post"
+      ]
+
+     }).then(function(){
+
+     console.log("ingresa a redirecr");
+
+     res.render('respuestaPost', {mensaje:'Se registro con exito'});
+
+     });  
+
+}
 //escribir el codigo para devolver respuesta
 
 //Escribir el codigo para guardar los datos
